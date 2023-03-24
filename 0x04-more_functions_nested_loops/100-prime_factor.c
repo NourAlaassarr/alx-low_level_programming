3d1fe79adbb1e1eeff8025d1d37429caf17f7e8f
Nour alaassar <itsalaassar@gmail.com>
2023-03-24 04:34:06 +0200

    hi

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,46 +1,25 @@
-#include <stdio.h>
-#include <math.h>
-
+#include "main.h"
 /**
- * main - finds and prints the largest prime factor of the number 612852475143
- * followed by a new line
- * @num: number to find
- * @x: input number
- * Return: Always 0 (Success)
+ * main - prime numbers
+ *
+ * Return: 0
  */
-double _sqrt(double x)
-{
-	float sqrt , tmp;
-	sqrt = x / 2;
-	tmp = 0;
-	while(sqrt != tmp)
-	{
-		tmp = sqrt;
-		sqrt = (x / tmp + tmp) / 2;
-	}
-	return (sqrt);
-}
-
-void largest_prim_factor(long int num)
-{
-	int prmnu, largest;
-	while(num % 2 == 0)
-		num = num /2;
-	for(prmnu = 3; prmnu <= _sqrt(num); prmnu +=2)
-	{
-		while (num % prmnu == 0)
-		{
-			num = num/ prmnu;
-			largest = prmnu;
-		}
-	}
-	if (num > 2)
-		largest = num;
-	printf("%d" , largest);
-}
 
 int main(void)
 {
-largest_prim_factor(612852475143);
-return(0);
+	long num = 612852475143;
+	long divisor = 2;
+	long larg_prim = 0;
+
+	while (num != 1)
+	{
+		if (num % divisor == 0)
+		{
+			num = num / divisor;
+			larg_prim = divisor;
+		}
+		divisor += 1;
+	}
+	printf("%ld\n", larg_prim);
+	return (0);
 }
