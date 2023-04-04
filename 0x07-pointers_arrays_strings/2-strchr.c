7047fc287762dbcd5ec7f85fd237f16d9552d49e
Nour alaassar <itsalaassar@gmail.com>
2023-04-04 21:09:59 +0200

    task 2 again

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * _strchr - Entry point ch
- * @s: input
- * @c: input
- * Return: Always 0
+ * _strchr -  Function
+ * @s: char one
+ * @c: char two
+ * Return: NULL (SUCCESS)   if not found
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-	for (; s[i] >= '\0'; i++)
-	{
-		if(s[i] == c)
-			return(&s[i]);
-	}
-	return(0);
+int i;
+for (i = 0; s[i] != '0'; i++)
+{
+if (s[i]  == c)
+return (s + i);
+}
+return (NULL);
 }
