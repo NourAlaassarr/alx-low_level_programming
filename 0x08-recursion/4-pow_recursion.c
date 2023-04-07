7426516a3fe29f22545e94459def361e4cfcc0e8
Nour alaassar <itsalaassar@gmail.com>
2023-04-07 02:37:38 +0200

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,4 +1,4 @@
-
+#include "main.h"
 /**
 * _pow_recursion - Entry Function
  * @x: integer
@@ -13,7 +13,7 @@ return (-1);
 else if (y == 1)
 return (x);
 else if (y == 0)
-return(0);
+return (1);
 else
 return (x * _pow_recursion(x, y - 1));
 }
