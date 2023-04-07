1f8745216cd5dd1265c5f8aabe5e49cb39911271
Nour alaassar <itsalaassar@gmail.com>
2023-04-07 02:34:32 +0200

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,15 +1,17 @@
+
 /**
- * _pow_recursion - returns the value of x power to y
- * @x: the base 
- * @y: the power
- * Return: the value of x power of y, or -1 if y is smaller than 0
+* _pow_recursion - Entry Function
+ * @x: integer
+ * @y: integer
+ * Return: value of the exponentiation.
  */
+
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-        return -1;
-    else if (y == 0)
-        return 1;
-    else
-        return x * _pow_recursion(x, y - 1);
+if (y < 0)
+return (-1);
+else if (y == 1)
+return (x);
+else
+return (x * _pow_recursion(x, y - 1));
 }
