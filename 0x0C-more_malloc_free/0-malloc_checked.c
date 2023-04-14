53ac82d355fb8ecfb4e1e2bb2b399b5162ebc82f
Nour alaassar <itsalaassar@gmail.com>
2023-04-14 02:23:08 +0200

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *malloc_check - function 
+ * @b: number
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+	p = malloc(b * sizeof(char));
+	if (p == NULL)
+		exit (98);
+	else
+		return (p);
+}
