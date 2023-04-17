e40203291a6cf32f6f68186ff5a00f19ea54a409
Nour alaassar <itsalaassar@gmail.com>
2023-04-17 07:17:45 +0200

    hello

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,13 +7,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	if(argc > 0)
+int i;
+
+	for (i = 0; i < argc; i++)
 	{
-		for(i = 0; i < argc; i++)
-		{ printf("%s\n", argv[i]);
-		}
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
-
