5506e812947cc2ade492ae04a76fa67fa11c6d22
Nour alaassar <itsalaassar@gmail.com>
2023-04-19 03:43:04 +0200

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a new type
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
