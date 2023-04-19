5d7e11989bfe997ec9374627394ae95723617380
Nour alaassar <itsalaassar@gmail.com>
2023-04-19 02:07:39 +0200

    task 4

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,13 +1,15 @@
 #ifndef SUM_H
 #define SUM_H
+ 
 
 /**
  * main - Entry point
- * Descr: Header
- * SUM(X, Y), sum
- * @x: first num
- * @y: sec num
+ * Descr: Header file that defines a function-like macro
+ * SUM(x, y), that computes
+ * @x: sum of the numbers
+ * @y: the sum of macro
  */
-#define SUM(x, y) ((x) +(y))
+
+#define SUM(x, y) ((x) + (y))
 
 #endif
