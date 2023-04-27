6bcd069a3a44027d21ea633e19605b726d9b3392
itsalaassar <itsalaassar@gmail.com>
2023-04-27 08:20:14 +0200

    score

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list p;
 	unsigned int i;
 	va_start(p, n);
-	for (i=0; i < n; i++)
+
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(p, int));
 		if ( i != (n - 1) && separator != NULL)
