13d5b866e92c52d558177066d519a7d2a10bf2da
itsalaassar <itsalaassar@gmail.com>
2023-04-27 08:25:02 +0200

    socr

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,12 +14,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list p;
 	unsigned int i;
-	va_start(p, n);
 
+	va_start(p, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(p, int));
-		if ( i != (n - 1) && separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
