be55d0e2e51ba0dea12e3d2a63b07a99126d43de
itsalaassar <itsalaassar@gmail.com>
2023-04-27 07:57:07 +0200

    score

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+
+
 /**
- * main -  prints all arguments it receives
- * argc: int 
- * argv:array of char
- * Return: always 0
+ * main - entry point
+ * @argc: char
+ * @argv: char *
+ * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
 int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-	return (0);
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
 }
