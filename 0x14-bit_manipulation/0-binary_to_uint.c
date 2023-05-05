2763f89737e90c92d4e4aec2cfcf093d38a5278f
itsalaassar <itsalaassar@gmail.com>
2023-05-05 18:31:07 +0300

    binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert binary to int
+ * @b: string
+ *
+ * Return: dicimal num
+ **/
+unsigned int binary_to_uint(const char *b)
+{
+    	int i;
+	unsigned int sum = 0;
+if (!b)
+return (0);
+	for (i = 0; b[i] ; i++)
+{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+    sum = (sum << 1) | (b[i] - '0');
+
+	}
+
+	return (sum);
+}
