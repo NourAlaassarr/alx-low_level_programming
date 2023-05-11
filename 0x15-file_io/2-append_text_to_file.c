81d2278a4436b9220e411cff0d606cad93aa0507
root <root@DESKTOP-3U6CORI.localdomain>
2023-05-11 18:21:19 +0300

    c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <fcntl.h>
+/**
+ * append_text_to_file - fun to append
+ * @filename:file name
+ * @text_content: string
+ * Return: 1 if succeed
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+int ptr, writein;
+int size = 0;
+
+if (filename == NULL)
+	return (-1);
+ptr = open(filename, O_WRONLY | O_APPEND);
+
+if (ptr < 0)
+	return (-1);
+if (text_content)
+{
+	while (text_content[size])
+		size++;
+	writein = write(ptr, text_content, size);
+	if (writein != size)
+		return (-1);
+}
+close(ptr);
+return (1);
+}
